2dc6493ff94591e7a7486077d8db592afa29c97e
softchris <noringc@gmail.com>
2020-01-09 16:51:20 +0000

    first page layout

---
+++ assets/js/157.466eea0a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[157],{9:function(n,w,o){}}]);
---
+++ assets/js/45.8fecc792.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[45],{283:function(t,n,e){"use strict";e.r(n);var s=e(0),o=Object(s.a)({},function(){var t=this.$createElement,n=this._self._c||t;return n("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[n("p",[this._v("Routing")])])},[],!1,null,null,null);n.default=o.exports}}]);
